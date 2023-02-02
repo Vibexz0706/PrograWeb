e7bd82f1b4920d93530123bd018e9aed8417fdde
José Julián Abarca Fallas <111944492+Vibexz0706@users.noreply.github.com>
2023-02-01 22:16:35 -0600

    TAREA
    COMMIT TAREA

---
+++ TAREA-EA/EA-08-PRACTICAS-2/src/test/java/TAREA2/PRACTICA2/Practica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package TAREA2.PRACTICA2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
