f2dbe4be9993aaf346b0f4fb4865d91816ff2920
José Julián Abarca Fallas <111944492+Vibexz0706@users.noreply.github.com>
2023-02-13 20:45:27 -0600

    MATERIASEMANA4
    MATERIA SEMANA 4

--- SEMANA 4/sem4/src/main/java/com/semana4/sem4/dao/ClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.semana4.sem4.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.semana4.sem4.domain.Cliente;
-
-public interface ClienteDao extends CrudRepository<Cliente, Long> {
-    
-}
