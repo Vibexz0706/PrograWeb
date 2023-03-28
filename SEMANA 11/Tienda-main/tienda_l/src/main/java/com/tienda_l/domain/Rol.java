d0077a8283167e2b4d73e87007e2da1473c3289f
José Julián Abarca Fallas <111944492+Vibexz0706@users.noreply.github.com>
2023-03-27 20:54:56 -0600

    SEMANA 11
    MATERIA SEMANA 11

---
+++ SEMANA 11/Tienda-main/tienda_l/src/main/java/com/tienda_l/domain/Rol.java
@@ -0,0 +1,24 @@
+package com.tienda_l.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="rol")
+public class Rol implements Serializable {
+   
+    private static final long serialVersionUID = 1L;
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_rol")
+    private Long idRol;
+   
+    @NotEmpty
+    private String nombre;
+   
+   
+}
